575fe6d901eef942fefac9bb6eb8a4aa510e6a93
olivier Pflieger <keryss@gmail.com>
2026-06-23 11:17:41 +0200

    feat: Implement EKS

--- infra/eks/modules/poc/main.tf
+++ infra/eks/modules/poc/main.tf
@@ -1,3 +1,9 @@
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource "aws_s3_bucket" "poc" {
   bucket = "project6-poc-${random_string.suffix.result}"
 }
