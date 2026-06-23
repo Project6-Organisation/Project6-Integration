45570dc8e85c6feccdb5a26945331514850f509e
olivier Pflieger <keryss@gmail.com>
2026-06-23 11:01:41 +0200

    feat: Implement EKS

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -15,23 +15,23 @@ module "vpc" {
 # Create EKS Cluster and Node Groups
 ################################################################################
 
-module "eks" {
-  source             = "./modules/eks"
-  public_subnets_id  = module.vpc.public_subnets_id
-  private_subnets_id = module.vpc.private_subnets_id
-  security_groups_id = module.vpc.security_groups_id
-  cluster_config     = var.cluster_config
-  common_tags        = local.common_tags
-  naming_prefix      = local.naming_prefix
-}
+# module "eks" {
+#   source             = "./modules/eks"
+#   public_subnets_id  = module.vpc.public_subnets_id
+#   private_subnets_id = module.vpc.private_subnets_id
+#   security_groups_id = module.vpc.security_groups_id
+#   cluster_config     = var.cluster_config
+#   common_tags        = local.common_tags
+#   naming_prefix      = local.naming_prefix
+# }
 
 ################################################################################
 # Update kubeconfig with created cluster
 ################################################################################
-resource "null_resource" "update_kubeconfig" {
-  provisioner "local-exec" {
-    command = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.cluster_config.name}"
-  }
-  depends_on = [module.eks]
-}
+# resource "null_resource" "update_kubeconfig" {
+#   provisioner "local-exec" {
+#     command = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.cluster_config.name}"
+#   }
+#   depends_on = [module.eks]
+# }
 
