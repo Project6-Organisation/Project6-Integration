bed21bcea50996c39c792f2861b7470110cffdde
KERYSSLAPTOP\kerys <keryss@gmail.com>
2026-06-24 15:19:46 +0200

    feat: implement https

--- infra/eks/outputs.tf
+++ infra/eks/outputs.tf
@@ -10,3 +10,15 @@ output "cluster_name" {
   description = "EKS cluster name"
   value       = module.eks.cluster_name
 }
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.eks-cluster.certificate_authority[0].data
+}
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.eks_oidc_provider.arn
+}
+
+output "oidc_provider_url" {
+  value = aws_iam_openid_connect_provider.eks_oidc_provider.url
+}
