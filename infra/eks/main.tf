b216812c53bb0b74520ad70af0482a55b540f188
olivier Pflieger <keryss@gmail.com>
2026-06-24 08:08:22 +0200

    feat: Test deployment cluster

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -2,47 +2,47 @@
 # POC to check if everything is ok
 ################################################################################
 
-module "poc" {
-  source      = "./modules/poc"
-  environment = var.environment
-}
+# module "poc" {
+#   source      = "./modules/poc"
+#   environment = var.environment
+# }
 
 ################################################################################
 # Create VPC and components
 ################################################################################
 
-# module "vpc" {
-#   source          = "./modules/vpc"
-#   networking      = var.networking
-#   security_groups = var.security_groups
-#   common_tags     = local.common_tags
-#   naming_prefix   = local.naming_prefix
-#   cluster_name    = var.cluster_config.name
-# }
+module "vpc" {
+  source          = "./modules/vpc"
+  networking      = var.networking
+  security_groups = var.security_groups
+  common_tags     = local.common_tags
+  naming_prefix   = local.naming_prefix
+  cluster_name    = var.cluster_config.name
+}
 
 ################################################################################
 # Create EKS Cluster and Node Groups
 ################################################################################
 
-# module "eks" {
-#   source             = "./modules/eks"
-#   public_subnets_id  = module.vpc.public_subnets_id
-#   private_subnets_id = module.vpc.private_subnets_id
-#   security_groups_id = module.vpc.security_groups_id
-#   cluster_config     = var.cluster_config
-#   common_tags        = local.common_tags
-#   naming_prefix      = local.naming_prefix
-#   node_groups        = var.node_groups
-#   environment        = var.environment
-# }
+module "eks" {
+  source             = "./modules/eks"
+  public_subnets_id  = module.vpc.public_subnets_id
+  private_subnets_id = module.vpc.private_subnets_id
+  security_groups_id = module.vpc.security_groups_id
+  cluster_config     = var.cluster_config
+  common_tags        = local.common_tags
+  naming_prefix      = local.naming_prefix
+  node_groups        = var.node_groups
+  environment        = var.environment
+}
 
 ################################################################################
 # Update kubeconfig with created cluster
 ################################################################################
 
-# resource "null_resource" "update_kubeconfig" {
-#   provisioner "local-exec" {
-#     command = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.cluster_config.name}"
-#   }
-#   depends_on = [module.eks]
-# }
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.cluster_config.name}"
+  }
+  depends_on = [module.eks]
+}
