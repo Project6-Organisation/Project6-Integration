538a869fdf12bae981f134322b0a627d5b01f599
olivier Pflieger <keryss@gmail.com>
2026-06-25 09:00:20 +0200

    fix: Split terraform eks and alb

--- infra/eks/modules/eks/aws-load-balancer-controller.tf
+++ infra/eks/modules/eks/aws-load-balancer-controller.tf
@@ -1,43 +1,43 @@
-resource "helm_release" "aws_load_balancer_controller" {
-  name       = "aws-load-balancer-controller"
-  namespace  = "kube-system"
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name       = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
 
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  version    = "1.8.1"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   version    = "1.8.1"
 
-  set {
-    name  = "clusterName"
-    value = aws_eks_cluster.eks-cluster.name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = aws_eks_cluster.eks-cluster.name
+#   }
 
-  set {
-    name  = "region"
-    value = var.aws_region
-  }
+#   set {
+#     name  = "region"
+#     value = var.aws_region
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.AWSLoadBalancerControllerRole.arn
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.AWSLoadBalancerControllerRole.arn
+#   }
 
-  set {
-    name  = "vpcId"
-    value = var.vpc_id
-  }
+#   set {
+#     name  = "vpcId"
+#     value = var.vpc_id
+#   }
 
-  depends_on = [
-    aws_eks_node_group.node-ec2,
-    aws_iam_role_policy_attachment.AWSLoadBalancerControllerPolicyAttachment
-  ]
-}
+#   depends_on = [
+#     aws_eks_node_group.node-ec2,
+#     aws_iam_role_policy_attachment.AWSLoadBalancerControllerPolicyAttachment
+#   ]
+# }
