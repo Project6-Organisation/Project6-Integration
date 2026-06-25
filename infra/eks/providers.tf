987f98da9a2b1786e8e5336c5ec2ed9c61c496b1
olivier Pflieger <keryss@gmail.com>
2026-06-25 07:36:56 +0200

    feat: fix destroy

--- infra/eks/providers.tf
+++ infra/eks/providers.tf
@@ -30,13 +30,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "eks" {
+#   name = module.eks.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = module.eks.cluster_name
+# }
 
 # provider "kubernetes" {
 #   host                   = data.aws_eks_cluster.eks.endpoint
