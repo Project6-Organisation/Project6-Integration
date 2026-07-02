8b754bf261571a2a1664d30327302ee6f42bdd7a
olivier Pflieger <keryss@gmail.com>
2026-07-02 09:40:05 +0200

    fix: Fix IAM

--- infra/eks/modules/eks/main.tf
+++ infra/eks/modules/eks/main.tf
@@ -90,48 +90,13 @@ resource "aws_eks_addon" "addons" {
   depends_on = [aws_eks_node_group.node-ec2]
 }
 
-################################################################################
-# AWS EBS CSI DRIVER ROLE
-################################################################################
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = aws_eks_cluster.eks-cluster.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
 
-data "aws_iam_policy_document" "ebs_csi_assume_role" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "sts:AssumeRoleWithWebIdentity"
-    ]
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.eks_oidc_provider.arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:sub"
-      values = [
-        "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:aud"
-      values = [
-        "sts.amazonaws.com"
-      ]
-    }
-  }
-}
-
-resource "aws_iam_role" "ebs_csi_driver" {
-  name = "${var.naming_prefix}-EBSCSIDriverRole"
-
-  assume_role_policy = data.aws_iam_policy_document.ebs_csi_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
-  role       = aws_iam_role.ebs_csi_driver.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  depends_on = [
+    aws_eks_node_group.node-ec2,
+    aws_iam_role_policy_attachment.ebs_csi_driver
+  ]
 }
